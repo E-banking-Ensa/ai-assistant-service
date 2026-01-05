8ff29af1cd262757dcdf227dd51e4543b58ca1c7
ed-dbibi <135333122+ed-dbibi@users.noreply.github.com>
2026-01-05 20:34:39 +0100

    chore: secure config using example file

---
+++ config.example.py
@@ -0,0 +1,4 @@
+import os
+API_KEY="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"
+DATABASE_URL = "sqlite:///bank.db"
+SECRET_KEY = "your_secret_key_here"
